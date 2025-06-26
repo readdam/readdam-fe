c6cef4cfb0b26cdb020dac6a64247891e203d311
mokkmokk <ramona.april@gmail.com>
2025-06-26 17:03:46 +0900

    doc: config 파일 교체

--- src/config/config.jsx
+++ src/config/config.jsx
@@ -1,4 +1,45 @@
+import axios from "axios";
+
 export const url = "http://localhost:8080";
+export const reactUrl = "http://localhost:5173";
+
+export const myAxios = (token, setToken) => {
+   var instance = axios.create({
+      baseURL : url,
+      timeout:5000,
+   })
+
+   instance.interceptors.response.use(
+      (response) => {
+         if(response.headers.authorization) {
+            setToken(response.headers.authorization)
+         }
+         return response;
+      }
+      ,
+      (error) => {
+         if (error.response && error.response.status) {
+            switch (error.response.status) {
+               case 401: 
+               case 403:
+                     window.location.href=`${reactUrl}/login`;  break;
+               default:
+               return Promise.reject(error);
+            }
+         }
+         return Promise.reject(error);
+      }
+   );
+
+   token && instance.interceptors.request.use((config)=>{
+    config.headers.Authorization = token;
+    return config;
+   });
+
+   return instance;
+}
+
+
 export const themeObj = {
    bgColor: "", 			// 바탕 배경색
    searchBgColor: "", 		// 검색창 배경색
@@ -10,4 +51,3 @@ export const themeObj = {
    emphTextColor: "", 		// 강조 글자색
    outlineColor: "" 		// 테두리
 };
-
