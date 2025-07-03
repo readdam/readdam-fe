e1ea96ff849b78bb6ccba66f5b8f61d3ed870332
mokkmokk <ramona.april@gmail.com>
2025-07-03 09:12:20 +0900

    feat: 모임 리스트 검색기능 작업중

--- src/components/class/ClassCard.jsx
+++ src/components/class/ClassCard.jsx
@@ -1,70 +1,72 @@
-import React from 'react';
-import { url } from '@config/config';
-import { useNavigate } from 'react-router';
+import React from "react";
+import { url } from "@config/config";
+import { useNavigate } from "react-router";
 import {
   HeartIcon,
   CalendarIcon,
   CompassIcon,
-} from 'lucide-react'
+  BookOpenIcon,
+} from "lucide-react";
 
-const ClassCard = ({group}) => {
-    const tagArray = [group.tag1, group.tag2, group.tag3].filter(Boolean);
-    const navigate = useNavigate();
+const ClassCard = ({ group }) => {
+  const tagArray = [group.tag1, group.tag2, group.tag3].filter(Boolean);
+  const navigate = useNavigate();
 
-    return (
-        <div
-            key={group.classId}
-            className="bg-white rounded-lg overflow-hidden shadow-md border border-gray-100 hover:shadow-lg transition-shadow"
-        >
-            <div className="h-48 overflow-hidden">
-                <img src={`${url}/image?filename=${group.mainImg}`}
-                    alt={group.title}
-                    className="w-full h-full object-cover"
-                />
-            </div>
-            <div className="p-4">
-                <div className="flex justify-between items-start mb-3">
-                    <h3 className="text-lg font-semibold text-gray-800">
-                        {group.title}
-                    </h3>
-                    <button className="flex items-center gap-1 text-gray-500">
-                        <HeartIcon
-                            className='w-5 h-5 fill-red-500 text-red-500'
-                        />
-                        {/* <span>{group.likeCount}</span> */}
-                    </button>
-                </div>
-                <div className="flex items-center text-gray-600 mb-2">
-                    <span className="text-sm">{group.shortIntro}</span>
-                </div>
-                <div className="flex flex-wrap gap-2 mb-3">
-                    {tagArray.map((tag, index) => (
-                        <span
-                            key={index}
-                            className="px-2 py-1 bg-[#F3F7EC] text-[#006989] text-sm rounded-full"
-                        >
-                            {tag}
-                        </span>
-                    ))}
-                </div>
-                <div className="flex items-center text-gray-600 mb-2">
-                    <CalendarIcon className="w-4 h-4 mr-2" />
-                    <span className="text-sm">시작일: &nbsp;</span>
-                    <span className="text-sm">{group.round1Date}</span>
-                </div>
-                <div className="flex items-center text-gray-600 mb-4">
-                    <CompassIcon className="w-4 h-4 mr-2" />
-                    <span className="text-sm">장소: &nbsp;</span>
-                    <span className="text-sm">{group.round1PlaceName}</span>
-                </div>
-                <button onClick={()=>{
-                    navigate(`/classDetail/${group.classId}`)
-                }} 
-                    className="w-full px-4 py-2 bg-[#006989] text-white rounded-lg hover:bg-[#005C78] transition-colors">
-                    자세히 보기
-                </button>
-            </div>
+  return (
+    <div
+      key={group.classId}
+      className="bg-white rounded-lg overflow-hidden shadow-md border border-gray-100 hover:shadow-lg transition-shadow"
+    >
+      <div className="h-48 overflow-hidden">
+        <img
+          src={`${url}/image?filename=${group.mainImg}`}
+          alt={group.title}
+          className="w-full h-full object-cover"
+        />
+      </div>
+      <div className="p-4">
+        <div className="flex justify-between items-start mb-3">
+          <h3 className="text-lg font-semibold text-gray-800">{group.title}</h3>
+          <button className="flex items-center gap-1 text-gray-500">
+            <HeartIcon className="w-5 h-5 text-red-500" />
+            <span>{group.likeCnt ?? 0}</span>
+          </button>
         </div>
-    );
+
+        <div className="flex flex-wrap gap-2 mb-3">
+          {tagArray.map((tag, index) => (
+            <span
+              key={index}
+              className="px-2 py-1 bg-[#F3F7EC] text-[#006989] text-sm rounded-full"
+            >
+              {tag}
+            </span>
+          ))}
+        </div>
+        <div className="flex items-center text-gray-600 mb-2">
+          <BookOpenIcon className="w-4 h-4 mr-2" />
+          <span className="text-sm">{group.shortIntro}</span>
+        </div>
+        <div className="flex items-center text-gray-600 mb-2">
+          <CalendarIcon className="w-4 h-4 mr-2" />
+          <span className="text-sm">시작일: &nbsp;</span>
+          <span className="text-sm">{group.round1Date}</span>
+        </div>
+        <div className="flex items-center text-gray-600 mb-4">
+          <CompassIcon className="w-4 h-4 mr-2" />
+          <span className="text-sm">장소: &nbsp;</span>
+          <span className="text-sm">{group.round1PlaceName}</span>
+        </div>
+        <button
+          onClick={() => {
+            navigate(`/classDetail/${group.classId}`);
+          }}
+          className="w-full px-4 py-2 bg-[#006989] text-white rounded-lg hover:bg-[#005C78] transition-colors"
+        >
+          자세히 보기
+        </button>
+      </div>
+    </div>
+  );
 };
 export default ClassCard;
