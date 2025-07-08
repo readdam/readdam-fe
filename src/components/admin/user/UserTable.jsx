9bba1b53b71ea32bdd53d426edfa113499566fd0
mokkmokk <ramona.april@gmail.com>
2025-07-09 04:12:54 +0900

    feat: 관리자> 회원조회 기능 구현

--- src/components/admin/user/UserTable.jsx
+++ src/components/admin/user/UserTable.jsx
@@ -1,6 +1,5 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import { url } from '@config/config';
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const UserTable = ({ members }) => {
   const navigate = useNavigate();
@@ -11,35 +10,55 @@ const UserTable = ({ members }) => {
 
   return (
     <div className="bg-white rounded-lg shadow-sm overflow-hidden">
-    <table className="w-full">
-      <thead className="bg-gray-50">
-        <tr>
-          <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">No</th>
-          <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">아이디</th>
-          <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">닉네임</th>
-          <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">이메일</th>
-          {/* <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">가입일</th> */}
-        </tr>
-      </thead>
-      <tbody className="bg-white divide-y divide-gray-200">
-        {members.length === 0 ? (
+      <table className="w-full">
+        <thead className="bg-gray-50">
           <tr>
-            <td colSpan="8" style={{ textAlign: 'center' }}>검색 결과가 없습니다.</td>
+            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              No
+            </th>
+            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              아이디
+            </th>
+            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              닉네임
+            </th>
+            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              이메일
+            </th>
+            {/* <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">가입일</th> */}
           </tr>
-        ) : (
-          members.map((member, index) => (
-            <tr key={member.username} className="hover:bg-gray-50">
-              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">{index + 1}</td>
-              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">{member.username}</td>
-              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">{member.nickname}</td>
-              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">{member.email}</td>
-              {/* <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">{member.joinDate}</td> */}
-             
+        </thead>
+        <tbody className="bg-white divide-y divide-gray-200">
+          {members.length === 0 ? (
+            <tr>
+              <td colSpan="8" style={{ textAlign: "center" }}>
+                검색 결과가 없습니다.
+              </td>
             </tr>
-          ))
-        )}
-      </tbody>
-    </table>
+          ) : (
+            members.map((member, index) => (
+              <tr
+                key={member.username}
+                className="hover:bg-gray-50 cursor-pointer"
+              >
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                  {index + 1}
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                  {member.username}
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                  {member.nickname}
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                  {member.email}
+                </td>
+                {/* <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">{member.joinDate}</td> */}
+              </tr>
+            ))
+          )}
+        </tbody>
+      </table>
     </div>
   );
 };
